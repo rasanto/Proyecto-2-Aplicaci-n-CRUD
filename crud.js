40d7f49c88d94bdadb34ff023145845988bbd5be
rasantoe <rasantos.dwfc5@ucamp.utel.edu.mx>
2022-03-31 21:48:52 -0400

    estado 2

--- crud.js
+++ crud.js
@@ -1,15 +1,42 @@
 function onFormSubmit(){
     var formData = readFormData();
+    insetNewRecord(formData)
+    resetForm()
 }
 
 function readFormData() {
     var formData = {};
-    formData["nombre"] = document.getElementById("nombre").Value;
-    formData["grado"] = document.getElementById("grado").Value;
-    formData["direccion"] = document.getElementById("direccion").Value;
+    formData["nombre"] = document.getElementById("nombre").value;
+    formData["grado"] = document.getElementById("grado").value;
+    formData["direccion"] = document.getElementById("direccion").value;
     return formData;
 }
 
 function insetNewRecord(dato) {
-    var table = document.getElementById("datos_estudiante").getElementsByTagName('tbody')
+    var table = document.getElementById("datos_estudiante").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+     cell1 = newRow.insertCell(0);
+     cell1.innerHTML = dato.nombre;
+     cell2 = newRow.insertCell(1);
+     cell2.innerHTML = dato.grado;
+     cell3 = newRow.insertCell(2);
+     cell3.innerHTML = dato.direccion;
+     cell3 = newRow.insertCell(3);
+     cell3.innerHTML =  ` <input type="button" value="Edit" onClick="onEdit(this)">
+                          <input type="button" value="delite">`;
+}
+
+
+
+function resetForm() {
+    document.getElementById("nombre").value = "";
+    document.getElementById("grado").value = "";
+    document.getElementById("direccion").value = "";
+}
+
+function onEdit(tb) {
+    SelectedRow = tb.parentElement.parentElement
+    document.getElementById("nombre").value = SelectedRow.cells[0].innerHTML;
+    document.getElementById("grado").value = SelectedRow.cells[1].innerHTML;
+    document.getElementById("direccion").value = SelectedRow.cells[2].innerHTML;
 }
